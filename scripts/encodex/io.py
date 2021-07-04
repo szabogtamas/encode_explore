52ddf0bc9fdc164fac3dfbb9cb247fa7dba37367
szabogtamas <szabogtamas@gmail.com>
2021-07-04 19:46:24 +0200

    Refactored parsers to reduce repetition

--- scripts/encodex/io.py
+++ scripts/encodex/io.py
@@ -4,6 +4,50 @@ import numpy as np
 import pandas as pd
 from typing import Union
 
+
+def read_from_s3(
+    s3_path: str,
+    default_path: str,
+    local_path: Union[None, str] = None,
+    force_download: bool = False
+) -> tuple:
+    """
+    Helper function downloading a file from S3 if not yet present.
+    Parameters
+    ----------
+    s3_path
+        Path to the file on S3.
+    default_path
+        A default file name to use.
+    local_path
+        Local path where file should be saved or is already saved.
+    force_download
+        If manifest table should be downloaded even if file exists locally.
+    Returns
+    -------
+    A file path string and boolean value indicating file if file should be removed.
+    """
+
+    file_present = False
+    cleanup_file = False
+
+    # Default option is to download manifest file temporarily and remove it
+    if local_path is None:
+        local_path = default_path
+        cleanup_file = True
+    
+    # If there is already a file locally, with overlapping name, do not remove
+    if os.path.isfile(local_path):
+        file_present = True
+        cleanup_file = False
+    
+    # Download manifest only if not present already
+    if force_download or not file_present:
+        subprocess.run(["aws", "s3", "cp", s3_path, local_path, "--no-sign-request"])
+    
+    return local_path, cleanup_file
+
+
 def read_files_manifest(
     manifest_s3_path: str = "s3://encode-public/encode_file_manifest.tsv",
     manifest_local_path: Union[None, str] = None,
@@ -24,23 +68,13 @@ def read_files_manifest(
     A dataframe with metadata for all ENCODE data files.
     """
 
-    cleanup_manifest = False
-    manifest_present = False
-
-    # Default option is to download manifest file temporarily and remove it
-    if manifest_local_path is None:
-        manifest_local_path = "encode_file_manifest.tsv"
-        cleanup_manifest = True
+    # Download file from S3
+    manifest_local_path, cleanup_manifest = read_from_s3(
+        manifest_s3_path, "encode_file_manifest.tsv",
+        manifest_local_path, force_download
+    )
     
-    # If there is already a manifest file locally, overlapping the manifest name, do not remove
-    if os.path.isfile(manifest_local_path):
-        cleanup_manifest = False
-        manifest_present = True
-    
-    # Download manifest only if not present already
-    if force_download or not manifest_present:
-        subprocess.run(["aws", "s3", "cp", manifest_s3_path, manifest_local_path, "--no-sign-request"])
-    
+    # Parse file
     manifest_df = pd.read_csv(manifest_local_path, sep="\t")
 
     # Remove file if no file name was specified
@@ -48,6 +82,7 @@ def read_files_manifest(
         os.remove(manifest_local_path)
     
     return manifest_df
+
 
 def read_experiment_bw(
     bw_s3_path: str,
@@ -68,24 +103,14 @@ def read_experiment_bw(
     -------
     A bigwig object with signal in genomic ranges.
     """
-
-    cleanup_bw = False
-    bw_present = False
-
-    # Default option is to download file temporarily and remove it
-    if bw_local_path is None:
-        bw_local_path = "experiment.bigWig"
-        cleanup_bw = True
     
-    # If the file is already present locally, do not remove
-    if os.path.isfile(bw_local_path):
-        cleanup_bw = False
-        bw_present = True
+    # Download file from S3
+    bw_local_path, cleanup_bw = read_from_s3(
+        bw_s3_path, "experiment.bigWig",
+        bw_local_path, force_download
+    )
     
-    # Download file only if not present already
-    if force_download or not bw_present:
-        subprocess.run(["aws", "s3", "cp", bw_s3_path, bw_local_path, "--no-sign-request"])
-    
+    # Parse file
     bw = pyBigWig.open(bw_local_path)
 
     # Remove file if no file name was specified
